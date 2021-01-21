b31b9f58365f0ad82f4fa60af8b9b843e389a3e4
MBARK T3STO <mbark1236@gmail.com>
2021-01-21 21:40:01 +0100

    Add project files.

---
+++ MBRepo/IRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBRepo
+{
+    interface IRepo<Tcontext> where  Tcontext : DbContext
+    {
+        void GetAll<Table>();
+        void GetOne<Table>(object pkValue);
+
+    }
+}
---
+++ MBRepo/Repo.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using System.Linq;
+
+namespace MBRepo
+{
+    public class Repo<Tcontext>:IRepo<Tcontext> where Tcontext: DbContext
+    {
+        private Tcontext _Context { get; set; } = null;
+        private DbSet    _Table   { get; set; } = null;
+
+
+
+       
+
+        public void GetAll<Table>()
+        {
+            return _Context.Set<Table>().ToList();
+        }
+    }
+}
